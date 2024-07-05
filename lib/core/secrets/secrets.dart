5d1bde1f9de7e859906453566fa8af0bf0684178
Mahmoud_Madih <moodbeder150@gmail.com>
2024-07-06 02:43:34 +0300

    finishung the app with issue on the backebd supabase

--- lib/core/secrets/secrets.dart
+++ lib/core/secrets/secrets.dart
@@ -1,4 +1,5 @@
 class Appsecrets{
   static const String superSecretURL = "https://axqzdjwhqzmnoghearof.supabase.co";
-  static const String supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4cXpkandocXptbm9naGVhcm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwMzc2MDMsImV4cCI6MjAzNTYxMzYwM30.fPVdXKuJqDHaWNpIjPf66x1KFRLch88YKrmR11YYX78";
+  static const String supabaseKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF4cXpkandocXptbm9naGVhcm9mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjAwMzc2MDMsImV4cCI6MjAzNTYxMzYwM30.fPVdXKuJqDHaWNpIjPf66x1KFRLch88YKrmR11YYX78";
 }
